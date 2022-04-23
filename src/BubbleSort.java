10a486465f8c944482b26a704174498636672537
Shao Doo <ahdoo.ling010519@gmail.com>
2022-04-23 19:27:52 +0800

    Define Counter class and implement counter in bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,23 +1,36 @@
 import java.io.*;
 import java.util.*;
 
+//class for counter
+class Counter{
+    int counter = 0;
+}
+
 public class BubbleSort {
     public static void main(String[] args) throws FileNotFoundException {
-        String[] array = new String[100];
-        readFile(array, "wordList100.txt");
-        String[] sortedArray = bubbleSort(array, array.length);
+        Counter counter = new Counter();
+        String[] array = new String[142301];
+        readFile(array, "wordList.txt");
+        String[] sortedArray = bubbleSort(array, array.length, counter);
         for(int i = 0; i < sortedArray.length; i++){
             System.out.println(i + 1 + ".\t" + sortedArray[i]);
         }
+        System.out.println(counter.counter);
     }
 
-    public static String[] bubbleSort(String[] array, int n){
+    public static String[] bubbleSort(String[] array, int n, Counter counter){
+        counter.counter = 1;           //int i = 0
         for(int i = 0; i < n; i++){
+            counter.counter += 3;      //i < n and i++
+            counter.counter += 2;      //int j = i + 1
             for(int j = i + 1; j < n; j++){
+                counter.counter += 3;  //j < n and j++
+                counter.counter += 3;  //comparing array[i] with array[j]
                 if(array[i].compareToIgnoreCase(array[j]) > 0){
                     String temp = array[i];
                     array[i]=array[j];
                     array[j]= temp;
+                    counter.counter += 7;
                 }
             }
         }
@@ -36,5 +49,4 @@ public class BubbleSort {
         sc.close();
         return array;
     }
-
 }
